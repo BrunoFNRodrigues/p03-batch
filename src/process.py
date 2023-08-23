09da332a5c103de7a5528c642fab60c14754632d
BrunoFNRodrigues <bf17669@gmail.com>
2023-08-23 16:08:37 -0300

    process

--- src/process.py
+++ src/process.py
@@ -10,7 +10,7 @@ new_db[['year', 'month', 'day']] = db.date.str.split("-", expand = True)
 date_format = '%Y-%m-%d'
 new_db[['weekday']] = db['date'].apply(lambda x: pd.Series(datetime.strptime(str(x), date_format).weekday()))
 
-new_db = new_db.groupby(['store_id','day', 'month', 'year', 'weekday']).price.sum().reset_index()
+new_db = new_db.groupby(['store_id','year', 'month', 'day', 'weekday']).price.sum().reset_index()
 new_db.rename(columns={'price':'total_sales'}, inplace=True)
 print(new_db)
 new_db.to_parquet("../data/train-2023-08-01.parquet",index=False)
